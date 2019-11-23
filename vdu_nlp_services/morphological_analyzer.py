2556ba35cf63ad228097edeb525dc864fd8a7642
Aleksas Pielikis <ant.kampo@gmail.com>
2019-11-24 01:07:00 +0200

    addd validation

--- vdu_nlp_services/morphological_analyzer.py
+++ vdu_nlp_services/morphological_analyzer.py
@@ -4,13 +4,19 @@
 from requests import post
 from re import compile, sub
 import string 
+import sqlite3
 
 re_tag = compile(r'<([^<>]+)/>')
 re_param = compile(r'([^ =]+)(="([^"]+)")?')
 
 def alter_text(text):
-    spec_set = set(u'-\n –')
-    spec_chars_only = sub(u'[' + ''.join(set(text) - spec_set) + ']', '', text)
+    spec_set = set(list(u'-\n –'))
+
+    try:
+        chars = (''.join(set(text) - spec_set)).replace(']', r'\]').replace('[', r'\[')
+        spec_chars_only = sub(u'[' + chars  + ']', '', text) if chars else ''
+    except TypeError as e:
+        print(e)
     altered_text = text.replace(u'–', '-').replace(u'\n', ' ')
     return altered_text, spec_chars_only
 
@@ -27,7 +33,7 @@ def process_response(response_content, spec_chars_only):
         
         if 'space' in element:
             if spec_chars_only[i - offset] == '\n':
-                element = {'sep': '&10;'}
+                element = {'sep': '\n'}
         elif 'sep' in element and element['sep'] == '-':
             if spec_chars_only[i - offset] == u'–':
                 element['sep'] = u'–'
@@ -37,6 +43,23 @@ def process_response(response_content, spec_chars_only):
         elements.append(element)
     
     return elements
+
+def validate(text, elements):
+    reconstructed = ''
+    for e in elements:
+        if 'word' in e:
+            reconstructed += e['word']
+        elif 'space' in e:
+            reconstructed += ' '
+        elif 'sep' in e:
+            if e['sep'] == None:
+                pass
+            else:
+                reconstructed += e['sep']
+        elif 'number' in e:
+            reconstructed += e['number']
+        else:
+            NotImplemented
 
 def analyze_text(text):
     altered_text, spec_chars_only = alter_text(text)
@@ -53,11 +76,26 @@ def analyze_text(text):
     if response.status_code != 200:
         raise Exception(response.reason)
 
-    return process_response(response.content, spec_chars_only)
+    elements = process_response(response.content, spec_chars_only)
+
+    validate(text, elements)
+    
+    return elements
 
 if __name__ == "__main__":
-    print (analyze_text('laba\n–--–-diena'))
-    print (analyze_text('Laba diena–draugai!\nKaip\njums -sekasi? Vienas, du, trys.'))
-    print (analyze_text('namo'))
-    print (analyze_text('Šioje vietoje trūksta namo!'))
-    print (analyze_text('Einam namo. Nerandu namo.'))
+    #print (analyze_text('laba\n–--–-diena'))
+    #print (analyze_text('Laba diena–draugai!\nKaip\njums -sekasi? Vienas, du, trys.'))
+    #print (analyze_text('namo'))
+    #print (analyze_text('Šioje vietoje trūksta namo!'))
+    #print (analyze_text('Einam namo. Nerandu namo.'))
+
+    conn = sqlite3.connect('data3.sqlite.db')
+    cursor = conn.cursor()
+
+    cursor.execute('SELECT article_id, `index`, block FROM article_blocks WHERE article_id > 1082')
+
+    for article_id, index, block in cursor:
+        print (article_id, index)
+        analyze_text(block)
+
+    conn.close()
