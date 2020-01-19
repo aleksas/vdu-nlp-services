bf891e9a5ae45a0c2142fdde851c3b590a1a8877
Aleksas Pielikis <ant.kampo@gmail.com>
2020-01-19 16:48:12 +0200

    Fix init.

--- vdu_nlp_services/__init__.py
+++ vdu_nlp_services/__init__.py
@@ -1,5 +1,5 @@
 from .morphological_analyzer import analyze_text
-from .fused_stressor import fused_stress_text, fused_stress_replacements, rebuild_text, localize_stressed_text, compare_replacements
+from .fused_stressor import fused_stress_text, fused_stress_replacements, rebuild_text
 from .soap_stressor import stress_text
 
-__version__ = '0.0.15'
+__version__ = '0.0.16'
