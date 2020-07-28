98817b34da00679886e3b9978455c13fa328ed2f
Aleksas Pielikis <ant.kampo@gmail.com>
2020-07-28 12:02:51 +0300

    Update tests.

--- tests/test_soap_stressor.py
+++ tests/test_soap_stressor.py
@@ -6,18 +6,18 @@ from vdu_nlp_services import analyze_text, stress_text, stress_word
 
 class SimpleTestCase(TestCase):
     def test_1(self):
-        stress_text('Laba diena draugai! Kaip jums sekasi? Vienas, du, trys.')
-        stress_text('namo')
-        stress_text('Šioje vietoje trūksta namo!')
-        stress_text('Einam namo!')
+        stress_text(u'Laba diena draugai! Kaip jums sekasi? Vienas, du, trys.')
+        stress_text(u'namo')
+        stress_text(u'Šioje vietoje trūksta namo!')
+        stress_text(u'Einam namo!')
 
     def test_2(self):
-        stress_word('Laba')
-        stress_word('keliai')
+        stress_word(u'Laba')
+        stress_word(u'keliai')
 
     def test_3(self):
-        res = [(x[0], list(x[1])) for x in stress_word('toj')]
-        expected = [('to~j', ['įvrd.', 'mot.gim.', 'vnsk.', 'Vt.', 'neįvardž.', 'sutrmp.'])]
+        res = [(x[0], list(x[1])) for x in stress_word(u'toj')]
+        expected = [(u'to~j', [u'įvrd.', u'mot.gim.', u'vnsk.', u'Vt.', u'neįvardž.', u'sutrmp.'])]
         self.assertEqual(res, expected)
 
 if __name__ == '__main__':
