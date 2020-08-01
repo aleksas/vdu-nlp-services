18ae91784bda14bee3a6bf7c675f55dceeb6117d
Aleksas Pielikis <ant.kampo@gmail.com>
2020-08-01 08:46:13 +0300

    Update __init__.py

--- vdu_nlp_services/__init__.py
+++ vdu_nlp_services/__init__.py
@@ -2,4 +2,4 @@ from .morphological_analyzer import analyze_text
 from .fused_stressor import fused_stress_text, fused_stress_replacements, rebuild_text
 from .soap_stressor import stress_text, stress_word
 
-__version__ = '0.0.26'
+__version__ = '0.0.27'
